41dc2287c657b4f8e7011cbb8379aaec06aed98b
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-11-25 23:57:13 +0100

    change

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import { HomeIcon, Menu } from 'lucide-react'
 // import Menu from '@/assets/icons/menu.svg'
+import { MenuIcon } from '@/components/icons/MenuIcon'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
@@ -25,7 +26,7 @@ function Sidebar() {
         <div className={`fixed top-0 bottom-0 ${isExpanded ? "left-0 " : "left-[-205px]"} w-[240px] bg-accent-foreground pt-[70px] border-r-2 border-r-foreground`}>
             <button className='absolute right-[-20px] mt-[14px] z-20 rounded-[50%] w-[40px] h-[40px] bg-foreground p-[10px] cursor-pointer border-1 border-accent'
                 onClick={() => isExpanded ? setIsExpanded(false) : setIsExpanded(true)}>
-                <Menu className='w-full h-full' />
+                <MenuIcon className='w-full h-full' />
             </button>
             {isExpanded &&
                 <nav className='flex flex-col w-full text-accent p-3 pt-[13px] gap-1'>

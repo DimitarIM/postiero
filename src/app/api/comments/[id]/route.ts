d6fc0724eafec5e7581032024d88db71517283d3
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-11-25 22:26:45 +0100

    ..

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -1,72 +1,75 @@
+import { NextRequest, NextResponse } from "next/server";
 import { verifySession } from "@/lib/auth/dal";
 import { createBackClient } from "@/utils/supabase/server";
-import { NextRequest, NextResponse } from "next/server";
 
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const { params } = context;
+    const checkedSession = await verifySession();
+    if (!checkedSession)
+      return NextResponse.json({ error: "Not Verified" }, { status: 401 });
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const checkedSession = await verifySession();
-        if (!checkedSession) return NextResponse.json({ error: "Not Verified" }, { status: 401 });
+    const { content } = await req.json();
 
-        const { content } = await req.json();
+    const supabase = await createBackClient();
 
-        const supabase = await createBackClient();
+    const { data, error } = await supabase
+      .from("posts")
+      .update({ content })
+      .eq("id", params.id);
 
-        const {id} = await params;
-
-        const { data, error } = await supabase
-            .from("posts")
-            .update({ content: content })
-            .eq('id', id);
-
-        if (error) {
-            console.error("Error updating:", error.message);
-            return NextResponse.json(
-                { error: error.message, success: false },
-                { status: 400 }
-            );
-        }
-        return NextResponse.json(
-            { message: "Comment Updated!", success: true, data: data },
-            { status: 201 })
-
-
-    } catch (err) {
-        console.error("Unexpected Error: ", err);
-        return NextResponse.json({ error: "Unexpected Error", success: false }, { status: 500 })
+    if (error) {
+      console.error("Error updating:", error.message);
+      return NextResponse.json(
+        { error: error.message, success: false },
+        { status: 400 }
+      );
     }
+
+    return NextResponse.json(
+      { message: "Comment Updated!", success: true, data },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Unexpected Error:", err);
+    return NextResponse.json(
+      { error: "Unexpected Error", success: false },
+      { status: 500 }
+    );
+  }
 }
 
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  try {
+    const { params } = context;
+    const checkedSession = await verifySession();
+    if (!checkedSession)
+      return NextResponse.json({ error: "Not Verified" }, { status: 401 });
 
+    const supabase = await createBackClient();
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    try {
-        const checkedSession = await verifySession();
-        if (!checkedSession) return NextResponse.json({ error: "Not Verified" }, { status: 401 });
+    const { data, error } = await supabase
+      .from("comment")
+      .delete()
+      .eq("id", params.id);
 
-        const supabase = await createBackClient();
-
-        const {id} = await params;
-
-        const { data, error } = await supabase
-            .from("comment")
-            .delete()
-            .eq('id', id);
-
-        if (error) {
-            console.error("Error deleting:", error.message);
-            return NextResponse.json(
-                { error: error.message, success: false },
-                { status: 400 }
-            );
-        }
-        return NextResponse.json(
-            { message: "Comment Deleted!", success: true, data: data },
-            { status: 201 })
-
-
-    } catch (err) {
-        console.error("Unexpected Error: ", err);
-        return NextResponse.json({ error: "Unexpected Error", success: false }, { status: 500 })
+    if (error) {
+      console.error("Error deleting:", error.message);
+      return NextResponse.json(
+        { error: error.message, success: false },
+        { status: 400 }
+      );
     }
+
+    return NextResponse.json(
+      { message: "Comment Deleted!", success: true, data },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("Unexpected Error:", err);
+    return NextResponse.json(
+      { error: "Unexpected Error", success: false },
+      { status: 500 }
+    );
+  }
 }

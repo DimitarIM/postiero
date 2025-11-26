13301890e7c0760559a846648ade700d633ff151
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-11-26 13:06:58 +0100

    fixed refresh at update page

--- src/app/(pages)/post/[id]/update/page.tsx
+++ src/app/(pages)/post/[id]/update/page.tsx
@@ -2,20 +2,23 @@
 
 import PostUpdateForm from '@/components/forms/PostUpdateForm'
 import { usePostStore } from '@/store/usePostStore';
+import { Loader2 } from 'lucide-react';
 import { useParams } from 'next/navigation'
 import React, { useEffect } from 'react'
 
 function Update() {
 
-    const { selectedPost, fetchPost } = usePostStore();
+    const { selectedPost, fetchPost, loading } = usePostStore();
     const params = useParams();
 
     useEffect(() => {
         fetchPost(params.id as string);
     },[]);
     return (
-        <div className='pt-[70px] min-h-screen'>
-            <PostUpdateForm  post={selectedPost!}/>
+        <div className='pt-[70px] min-h-screen justify-center'>
+            {
+                selectedPost &&<PostUpdateForm  post={selectedPost}/>
+            }
         </div>
 
     )
